f05fe01482caf07d8d980023916bdaafb4e152b3
IriarteMateo <miriarte@udesa.edu.ar>
2023-11-16 17:05:41 -0300

    comienzo de perfil

--- my-app/src/componentes/PostForm/PostForm.js
+++ my-app/src/componentes/PostForm/PostForm.js
@@ -1,78 +1,81 @@
-import react,{Component} from "react";
-import {db,auth} from '../../firebase/config';
-import { TextInput, TouchableOpacity, View,Text,StyleSheet } from "react-native";
+import react, { Component } from "react";
+import { db, auth } from '../../firebase/config';
+import { TextInput, TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
 class FormularioPost extends Component {
-        constructor(){
-            super()
-            this.state = {
-                descripcionPost :'',
-                url:'',
-            }
+    constructor() {
+        super()
+        this.state = {
+            descripcionPost: '',
+            url: '',
         }
+    }
 
-        creacionPost(autor,descripcionPost, createdAt){
-            db.collection('posteos').add({
-                autor: autor,
-                descripcionPost: descripcionPost,
-                createdAt: createdAt,
-                likes:[]
-            })
-            .then( res => console.log(res))
-            .catch (error=> console.log(error))
-        }
+    creacionPost(autor, descripcionPost, createdAt) {
+        db.collection('posteos').add({
+            autor: autor,
+            descripcionPost: descripcionPost,
+            createdAt: createdAt,
+            likes: []
+        })
+            .then(res => console.log(res))
+            .catch(error => console.log(error))
+    }
 
-        
 
-        render(){
-            return(
-                <View style ={style.formContainer}>
-                <TouchableOpacity>
+
+    render() {
+        return (
+            <View style={style.formContainer}>
                 <Text>New Post</Text>
-                </TouchableOpacity>
                 <TextInput
-                    style={style.input} 
-                    onChangeText={(text)=>this.setState({descripcionPost: text})}
+
+                    style={style.input}
+                    onChangeText={(text) => this.setState({ descripcionPost: text })}
                     placeholder='Escribite algo ...'
                     keyboardType='default'
-                    value={this.state.descripcionPost}/>
-                 
-                </View>
-            )
-        }
+                    value={this.state.descripcionPost} />
+
+                <TouchableOpacity style= {style.button}onPress={() => this.creacionPost(this.state.descripcionPost)}>
+                    <Text> Compartir </Text>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
 }
 const style = StyleSheet.create({
-    formContainer:{
-        paddingHorizontal:10,
+    formContainer: {
+        paddingHorizontal: 10,
         marginTop: 20,
         marginBottom: 40,
-        marginHorizontal:30,
+        marginHorizontal: 30,
         padding: 10,
         borderWidth: 1,
         borderColor: "#ccc",
         borderRadius: 6,
     },
-    input:{
-        height:20,
-        paddingVertical:15,
+    input: {
+        height: 20,
+        paddingVertical: 15,
         paddingHorizontal: 10,
-        borderWidth:1,
+        borderWidth: 1,
         borderColor: '#ccc',
         borderStyle: 'solid',
         borderRadius: 6,
-        marginVertical:10,
+        marginVertical: 10,
     },
-    button:{
-        backgroundColor:'#28a745',
+    button: {
+        backgroundColor: '#28a745',
         paddingHorizontal: 10,
         paddingVertical: 6,
         textAlign: 'center',
-        borderRadius:4, 
-        borderWidth:1,
+        borderRadius: 4,
+        borderWidth: 1,
         borderStyle: 'solid',
         borderColor: '#28a745'
     },
-    textButton:{
+    textButton: {
         color: '#fff'
     }
 
--- my-app/src/screens/Perfil/Perfil.js
+++ my-app/src/screens/Perfil/Perfil.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react'
-import { TouchableOpacity, View, Text} from 'react-native'
+import { TouchableOpacity, View, Text } from 'react-native'
+import { auth, db } from '../../firebase/config'
 
 
 class Perfil extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            posts: [],
+            info: {},
+            id: ''
+        }
     }
+
+    componentDidMount() {
+        db.collection('users')
+            .where('mail', '==', auth.currentUser.email)
+            .onSnapshot(info => {
+                info.forEach(info =>
+                    this.setState({
+                        id: info.id,
+                        info: info.data()
+                    })
+                )
+            })
+    }
+
 
 
     render() {
         return (
+
             <View>
                 <Text> Bienvendio a tu Perfil! </Text>
-                <TouchableOpacity onPressOut={() => this.props.navigation.navigate('Home')}>
-                    <Text> Volver al Home </Text>
-                </TouchableOpacity>
+                <Text> Biografía del usuario: {this.state.info.bio}</Text>
+                <Text> Tu email: {auth.currentUser.email}</Text>
+                <Text> Tu perfil se creó: {auth.currentUser.metadata.creationTime}</Text>
             </View>
 
         )
